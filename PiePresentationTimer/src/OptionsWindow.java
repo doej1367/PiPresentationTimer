fe90a276308c36a9a76a71f6b82cb40168e2f849
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 14:41:56 +0200

    initial commit

---
+++ PiePresentationTimer/src/OptionsWindow.java
@@ -0,0 +1,140 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class OptionsWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField_m;
+	private JTextField textField_s;
+	private JTextField textField_h;
+
+	public OptionsWindow(PiePresentationTimer piePresentationTimer) {
+		setDefaultCloseOperation(HIDE_ON_CLOSE);
+		setBounds(100, 100, 450, 164);
+		setLocation(Toolkit.getDefaultToolkit().getScreenSize().width - 555, 30);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 36, 0, 86, 6, 86, 16, 86, 16, 0, 0 };
+		gbl_panel.rowHeights = new int[] { 20, 20, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
+
+		JLabel lblTimer_1 = new JLabel("Timer:");
+		GridBagConstraints gbc_lblTimer_1 = new GridBagConstraints();
+		gbc_lblTimer_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblTimer_1.anchor = GridBagConstraints.EAST;
+		gbc_lblTimer_1.gridx = 1;
+		gbc_lblTimer_1.gridy = 1;
+		panel.add(lblTimer_1, gbc_lblTimer_1);
+
+		textField_h = new JTextField();
+		textField_h.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField_h.setText("0");
+		GridBagConstraints gbc_textField_h = new GridBagConstraints();
+		gbc_textField_h.fill = GridBagConstraints.BOTH;
+		gbc_textField_h.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_h.gridx = 2;
+		gbc_textField_h.gridy = 1;
+		panel.add(textField_h, gbc_textField_h);
+		textField_h.setColumns(10);
+
+		JLabel lblH = new JLabel("h");
+		GridBagConstraints gbc_lblH = new GridBagConstraints();
+		gbc_lblH.fill = GridBagConstraints.BOTH;
+		gbc_lblH.insets = new Insets(0, 0, 5, 5);
+		gbc_lblH.gridx = 3;
+		gbc_lblH.gridy = 1;
+		panel.add(lblH, gbc_lblH);
+
+		textField_m = new JTextField();
+		textField_m.setText("0");
+		textField_m.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_textField_m = new GridBagConstraints();
+		gbc_textField_m.fill = GridBagConstraints.BOTH;
+		gbc_textField_m.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_m.gridx = 4;
+		gbc_textField_m.gridy = 1;
+		panel.add(textField_m, gbc_textField_m);
+		textField_m.setColumns(10);
+
+		JLabel lblMin = new JLabel("min");
+		GridBagConstraints gbc_lblMin = new GridBagConstraints();
+		gbc_lblMin.anchor = GridBagConstraints.WEST;
+		gbc_lblMin.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMin.gridx = 5;
+		gbc_lblMin.gridy = 1;
+		panel.add(lblMin, gbc_lblMin);
+
+		textField_s = new JTextField();
+		textField_s.setText("60");
+		textField_s.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_textField_s = new GridBagConstraints();
+		gbc_textField_s.fill = GridBagConstraints.BOTH;
+		gbc_textField_s.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_s.gridx = 6;
+		gbc_textField_s.gridy = 1;
+		panel.add(textField_s, gbc_textField_s);
+		textField_s.setColumns(10);
+
+		JLabel lblSec = new JLabel("sec");
+		GridBagConstraints gbc_lblSec = new GridBagConstraints();
+		gbc_lblSec.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSec.anchor = GridBagConstraints.WEST;
+		gbc_lblSec.gridx = 7;
+		gbc_lblSec.gridy = 1;
+		panel.add(lblSec, gbc_lblSec);
+
+		JButton btnApply = new JButton("Apply");
+		btnApply.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				piePresentationTimer.setTimerDuration(getTimerDuration());
+			}
+		});
+		contentPane.add(btnApply, BorderLayout.SOUTH);
+
+		JPanel panel_1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel_1, BorderLayout.NORTH);
+
+		JButton btnCloseApplication = new JButton("Close Application");
+		btnCloseApplication.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		btnCloseApplication.setHorizontalAlignment(SwingConstants.LEADING);
+		panel_1.add(btnCloseApplication);
+	}
+
+	public long getTimerDuration() {
+		try {
+			return Long.parseLong(textField_h.getText()) * 3600 + Long.parseLong(textField_m.getText()) * 60
+					+ Long.parseLong(textField_s.getText());
+		} catch (NumberFormatException ex) {
+			return 60;
+		}
+	}
+
+}
---
+++ PiePresentationTimer/src/PiePresentationTimer.java
@@ -0,0 +1,183 @@
+
+/*
+ * Backbone copied from this site:
+ * https://docs.oracle.com/javase/tutorial/uiswing/misc/trans_shaped_windows.html
+ * 
+ * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import java.awt.geom.Ellipse2D;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import static java.awt.GraphicsDevice.WindowTranslucency.*;
+import static java.util.concurrent.TimeUnit.*;
+
+public class PiePresentationTimer extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel panel;
+	private Canvas canvas;
+	private int intervall = 500;
+	private int current = 0;
+	private float max = 60 * 1000;
+	private PiePresentationTimer ppt = this;
+
+	public PiePresentationTimer() {
+		super("ShapedWindow");
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.rowWeights = new double[] { 1.0 };
+		gridBagLayout.columnWeights = new double[] { 1.0 };
+		getContentPane().setLayout(gridBagLayout);
+
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				setShape(new Ellipse2D.Double(0, 0, getWidth(), getHeight()));
+			}
+		});
+		setAlwaysOnTop(true);
+		setUndecorated(true);
+		setSize(101, 101);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation(screenSize.width - 105, 30);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		getContentPane().add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 50 };
+		gbl_panel.rowHeights = new int[] { 0, 0 };
+		gbl_panel.columnWeights = new double[] { 1.0 };
+		gbl_panel.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if (arg0.getButton() == MouseEvent.BUTTON1)
+					reset();
+				else if (arg0.getButton() == MouseEvent.BUTTON3) {
+					EventQueue.invokeLater(new Runnable() {
+						public void run() {
+							try {
+								OptionsWindow frame = new OptionsWindow(getPpt());
+								frame.setVisible(true);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				}
+			}
+		});
+		canvas = new Canvas();
+		GridBagConstraints gbc_canvas = new GridBagConstraints();
+		gbc_canvas.gridx = 0;
+		gbc_canvas.gridy = 0;
+		panel.add(canvas, gbc_canvas);
+
+		final Runnable beeper = new Runnable() {
+			public void run() {
+				current += intervall;
+				current %= max;
+				updateDrawing();
+			}
+		};
+		final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+		final ScheduledFuture<?> beeperHandle = scheduler.scheduleAtFixedRate(beeper, 100, intervall, MILLISECONDS);
+		scheduler.schedule(new Runnable() {
+			public void run() {
+				beeperHandle.cancel(true);
+			}
+		}, 60 * 60, HOURS);
+	}
+
+	private void reset() {
+		current = 0;
+	}
+
+	private void updateDrawing() {
+		validate();
+		repaint();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.setColor(new Color(255, 0, 0));
+		int sector = -(int) (current * 360 / max);
+		if (sector == 0)
+			sector = 360;
+		g.fillArc(0 - 1, 0 - 1, getWidth() + 2, getHeight() + 2, 90, sector);
+		g.setColor(new Color(0, 0, 0));
+		String s = "" + (int) (max / 1000);
+		g.drawString(s, 50 - (s.length() / 2) * 7, 55);
+	}
+
+	public static void main(String[] args) {
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = ge.getDefaultScreenDevice();
+		final boolean isTranslucencySupported = gd.isWindowTranslucencySupported(TRANSLUCENT);
+		if (!gd.isWindowTranslucencySupported(PERPIXEL_TRANSPARENT)) {
+			System.err.println("Shaped windows are not supported");
+			System.exit(0);
+		}
+		if (!isTranslucencySupported)
+			System.out.println("Translucency is not supported, creating an opaque window");
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				PiePresentationTimer sw = new PiePresentationTimer();
+				if (isTranslucencySupported) {
+					sw.setOpacity(0.6f);
+				}
+				sw.setVisible(true);
+			}
+		});
+	}
+
+	public PiePresentationTimer getPpt() {
+		return ppt;
+	}
+
+	public void setTimerDuration(long timerDuration) {
+		max = timerDuration * 1000;
+	}
+}
